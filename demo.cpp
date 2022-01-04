f778bc87ed95a85c6d36e4756e389adab89bb796
limeflavour <limeflavour@163.com>
2022-01-04 16:57:55 +0800

    修改了demo.cpp，对两个初始的字符串进行显示

--- demo.cpp
+++ demo.cpp
@@ -8,6 +8,9 @@ int main()
     std::string str1 = "第五编党派社团";
     std::string str2 = "第五编 兄7}氏社团";
 
+    std::cout << "str1: " << str1 << std::endl;
+    std::cout << "str2: " << str2 << std::endl;
+
     std::unique_ptr<LargestCommonSubstring> ptrLCS(new LargestCommonSubstring(str1, str2, true));
     int comSubStringNum = ptrLCS->process();
     std::cout << "两个字符串的最大公共子串所含字符数: " <<comSubStringNum << std::endl;
