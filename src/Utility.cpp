a1adaf3f85ac675b8082320390d30c8c2bec1d3e
zxl <limeflavour@163.com>
2022-01-04 21:42:55 +0800

    修改了string转wstring及wstring转string

--- src/Utility.cpp
+++ src/Utility.cpp
@@ -2,96 +2,73 @@
 // Created by zxl on 2022/1/4.
 //
 #include "Utility.h"
-#include <windows.h>
+//#include <windows.h>
+#include <string>
+#include <locale>
+#include <codecvt>
 
-std::string limeflavour::UTF8_To_string(const std::string& str)
+//std::string limeflavour::UTF8_To_string(const std::string& str)
+//{
+//    int nwLen = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, NULL, 0);
+//    wchar_t* pwBuf = new wchar_t[nwLen + 1];    //‰∏ÄÂÆöË¶ÅÂä†1Ôºå‰∏çÁÑ∂‰ºöÂá∫Áé∞Â∞æÂ∑¥
+//    memset(pwBuf, 0, nwLen * 2 + 2);
+//    MultiByteToWideChar(CP_UTF8, 0, str.c_str(), str.length(), pwBuf, nwLen);
+//    int nLen = WideCharToMultiByte(CP_ACP, 0, pwBuf, -1, NULL, NULL, NULL, NULL);
+//    char* pBuf = new char[nLen + 1];
+//    memset(pBuf, 0, nLen + 1);
+//    WideCharToMultiByte(CP_ACP, 0, pwBuf, nwLen, pBuf, nLen, NULL, NULL);
+//
+//    std::string strRet = pBuf;
+//
+//    delete[]pBuf;
+//    delete[]pwBuf;
+//    pBuf = NULL;
+//    pwBuf = NULL;
+//
+//    return strRet;
+//}
+////stringËΩ¨ÂÜôÂÖ•xml‰∏≠ÁöÑ‰∏≠Êñá
+//std::string limeflavour::string_To_UTF8(const std::string & str)
+//{
+//    int nwLen = ::MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, NULL, 0);
+//
+//    wchar_t * pwBuf = new wchar_t[nwLen + 1];//‰∏ÄÂÆöË¶ÅÂä†1Ôºå‰∏çÁÑ∂‰ºöÂá∫Áé∞Â∞æÂ∑¥
+//    ZeroMemory(pwBuf, nwLen * 2 + 2);
+//
+//    ::MultiByteToWideChar(CP_ACP, 0, str.c_str(), str.length(), pwBuf, nwLen);
+//
+//    int nLen = ::WideCharToMultiByte(CP_UTF8, 0, pwBuf, -1, NULL, NULL, NULL, NULL);
+//
+//    char * pBuf = new char[nLen + 1];
+//    ZeroMemory(pBuf, nLen + 1);
+//
+//    ::WideCharToMultiByte(CP_UTF8, 0, pwBuf, nwLen, pBuf, nLen, NULL, NULL);
+//
+//    std::string retStr(pBuf);
+//
+//    delete[]pwBuf;
+//    delete[]pBuf;
+//
+//    pwBuf = NULL;
+//    pBuf = NULL;
+//
+//    return retStr;
+//}
+
+//stringËΩ¨wtringÔºàÂÆΩÂ≠óÁ¨¶Ôºâ
+std::wstring limeflavour::string_To_wstring(std::string &str)
 {
-    int nwLen = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, NULL, 0);
-    wchar_t* pwBuf = new wchar_t[nwLen + 1];    //“ª∂®“™º”1£¨≤ª»ªª·≥ˆœ÷Œ≤∞Õ
-    memset(pwBuf, 0, nwLen * 2 + 2);
-    MultiByteToWideChar(CP_UTF8, 0, str.c_str(), str.length(), pwBuf, nwLen);
-    int nLen = WideCharToMultiByte(CP_ACP, 0, pwBuf, -1, NULL, NULL, NULL, NULL);
-    char* pBuf = new char[nLen + 1];
-    memset(pBuf, 0, nLen + 1);
-    WideCharToMultiByte(CP_ACP, 0, pwBuf, nwLen, pBuf, nLen, NULL, NULL);
+    using convert_typeX = std::codecvt_utf8<wchar_t>;
+    std::wstring_convert<convert_typeX, wchar_t> converterX;
 
-    std::string strRet = pBuf;
-
-    delete[]pBuf;
-    delete[]pwBuf;
-    pBuf = NULL;
-    pwBuf = NULL;
-
-    return strRet;
+    return converterX.from_bytes(str);
 }
-//string◊™–¥»Îxml÷–µƒ÷–Œƒ
-std::string limeflavour::string_To_UTF8(const std::string & str)
+//wstring(ÂÆΩÂ≠óÁ¨¶)ËΩ¨string
+std::string limeflavour::wstring_To_string(std::wstring &wstr)
 {
-    int nwLen = ::MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, NULL, 0);
+    using convert_typeX = std::codecvt_utf8<wchar_t>;
+    std::wstring_convert<convert_typeX, wchar_t> converterX;
 
-    wchar_t * pwBuf = new wchar_t[nwLen + 1];//“ª∂®“™º”1£¨≤ª»ªª·≥ˆœ÷Œ≤∞Õ
-    ZeroMemory(pwBuf, nwLen * 2 + 2);
-
-    ::MultiByteToWideChar(CP_ACP, 0, str.c_str(), str.length(), pwBuf, nwLen);
-
-    int nLen = ::WideCharToMultiByte(CP_UTF8, 0, pwBuf, -1, NULL, NULL, NULL, NULL);
-
-    char * pBuf = new char[nLen + 1];
-    ZeroMemory(pBuf, nLen + 1);
-
-    ::WideCharToMultiByte(CP_UTF8, 0, pwBuf, nwLen, pBuf, nLen, NULL, NULL);
-
-    std::string retStr(pBuf);
-
-    delete[]pwBuf;
-    delete[]pBuf;
-
-    pwBuf = NULL;
-    pBuf = NULL;
-
-    return retStr;
-}
-
-//string◊™wtring£®øÌ◊÷∑˚£©
-std::wstring limeflavour::string_To_wstring(std::string & sTitle)
-{
-    int dwMinSize = MultiByteToWideChar(CP_ACP, 0, sTitle.c_str(), -1, NULL, 0);
-    wchar_t * pwcTitle = new wchar_t[dwMinSize];
-    if (pwcTitle == NULL)
-    {
-        //cerr << "ƒ⁄¥Ê∑÷≈‰ ß∞‹£°" << endl;
-        return L"";
-    }
-    MultiByteToWideChar(CP_ACP, 0, sTitle.c_str(), -1, pwcTitle, dwMinSize);
-    if (dwMinSize == 0)
-    {
-        //cerr << "ƒ⁄¥Ê∑÷≈‰ ß∞‹£°" << endl;
-        return L"";
-    }
-    std::wstring wsTitle(pwcTitle);
-    delete[]pwcTitle;
-    pwcTitle = NULL;
-    return wsTitle;
-}
-//wstring(øÌ◊÷∑˚)◊™string
-std::string limeflavour::wstring_To_string(std::wstring & wsTitle)
-{
-    int dwMinSize = WideCharToMultiByte(CP_ACP, NULL, wsTitle.c_str(), -1, NULL, 0, NULL, NULL);
-    char *pMulti = new char[dwMinSize];
-    if (pMulti == NULL)
-    {
-        //cerr << "ƒ⁄¥Ê∑÷≈‰ ß∞‹£°" << endl;
-        return "";
-    }
-    WideCharToMultiByte(CP_ACP, NULL, wsTitle.c_str(), -1, pMulti, dwMinSize, NULL, NULL);
-    if (dwMinSize == 0)
-    {
-        //cerr << "ƒ⁄¥Ê∑÷≈‰ ß∞‹£°" << endl;
-        return "";
-    }
-    std::string sTitle = pMulti;
-    delete[]pMulti;
-    pMulti = NULL;
-    return sTitle;
+    return converterX.to_bytes(wstr);
 }
 
